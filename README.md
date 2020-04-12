048f66ffb3c79cbd9e449a939b25eaa68c991c31
ralfz123 <ralf_zonneveld@hotmail.com>
2020-04-12 23:26:59 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# fe-assessment-2
+# Find Your Match! ❤️
+This feature is Progressively Enhanced.  
+It's built with HTML, CSS and JavaScript.  
+
+# Static prototype 🖥
+Go check my static prototype [here](https://ralfz123.github.io/fe-assessment-2/index.html).
+
+# Back-End feature
+Go check my feature repository with real Back-End ([NodeJS](https://nodejs.org/en/), [Express](https://expressjs.com/) and [MongoDB](https://www.mongodb.com/)) [here](https://github.com/ralfz123/prototype_datingapp). Read the [README](https://github.com/ralfz123/prototype_datingapp#find-your-match) to run the app.
+
+# MIT 👮
+[MIT](https://github.com/ralfz123/fe-assessment-2/blob/master/LICENSE)
--- script/profile.js
+++ script/profile.js
@@ -5,10 +5,11 @@ var enableButton = document.getElementById('enableButton');
 // Functions
 enableButton.onclick = () => {
     console.log('Clicked')
-for(var i = 0; i < inputs.length; i++) {
-    if (inputs[i].type == 'text' || inputs[i].type == 'password' || inputs[i].type == 'number' || inputs[i].type == 'radio'){
-        inputs[i].disabled = false;
-        console.log('Removed "disabled"')
+
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].type == 'text' || inputs[i].type == 'password' || inputs[i].type == 'number' || inputs[i].type == 'radio') {
+            inputs[i].disabled = false;
+            console.log('Removed "disabled"');
+        }
     }
-}
 }
