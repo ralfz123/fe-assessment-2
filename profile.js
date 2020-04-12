3233c5106dcf4b88117808ba9e40f59478af1532
ralfz123 <ralf_zonneveld@hotmail.com>
2020-04-12 22:05:17 +0200

    Deleted unused and added some files

---
+++ profile.js
@@ -0,0 +1,14 @@
+// var inputs = document.getElementsByTagName('input');
+// var enableButton = document.getElementsByClassName('enableButton');
+
+// enableButton.onclick = () => {
+//     console.log('geklikt')
+// for(var i = 0; i < inputs.length; i++) {
+//     if (inputs[i].type == 'text'){
+//         inputs[i].disabled = false;
+//     }
+// }
+// }
+
+// var enableButton = document.getElementsByClassName('enableButton');
+console.log('Hello world!')
