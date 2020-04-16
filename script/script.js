cd020e85de6254a9d1f486e7b77a8caff600f5b3
ralfz123 <ralf_zonneveld@hotmail.com>
2020-04-16 23:04:44 +0200

    Cleaned the structure of the code and added comments

--- script/script.js
+++ script/script.js
@@ -3,21 +3,18 @@ var fieldset = document.getElementsByTagName('fieldset');
 var button = document.getElementsByTagName('button');
 var li = document.getElementsByTagName('li');
 var form = document.getElementsByTagName('form');
-form.onsubmit = () => { return false };
 
-button[1].innerHTML = "Verder"
+button[1].innerHTML = "Verder"; // Changes from "Submit" to "Verder", because JS is now included (Progressive Enhancement)
 
-fieldset[1].style.display = "none";
-fieldset[2].style.display = "none";
+fieldset[1].style.display = "none"; // This fieldset is hidden, because the principle Progressive Disclosure
+fieldset[2].style.display = "none"; // This fieldset is hidden, because the principle Progressive Disclosure
 
 // When the current fieldset is equal to 0, then:
 var current_fieldset = 0;
 fieldset[current_fieldset].style.display = "block";
 if (current_fieldset == 0) {
-    // button[0].style.display = "none";
-    li[0].style.backgroundColor = "red";
-    button[0].style.display = "inline";
-}
+    li[0].style.backgroundColor = "red"; // The first bullet of the procesbar gets a red color
+};
 
 
 // When the right button ('Verder') gets hit
@@ -25,40 +22,37 @@ button[1].onclick = () => {
     current_fieldset++;
     var back_fieldset = current_fieldset - 1;
     if ((current_fieldset > 0) && (current_fieldset < 3)) {
-        button[1].style.display = "inline";
-        fieldset[current_fieldset].style.display = "block";
-        fieldset[back_fieldset].style.display = "none";
-        li[current_fieldset].style.backgroundColor = "red";
+        fieldset[current_fieldset].style.display = "block"; // This fieldset is now visible
+        fieldset[back_fieldset].style.display = "none"; // This fieldset is now invisible
+        li[current_fieldset].style.backgroundColor = "red"; // The current bullet of the procesbar gets a red color
         li[back_fieldset].style.backgroundColor = "#b3b3b3";
         if (current_fieldset == 2) {
-            button[1].innerHTML = "Submit";
-            button[1].setAttribute("type", "submit")
-            // form.onsubmit =()=>{return true};
-        }
-    }
-}
+            button[1].innerHTML = "Submit"; // Button text is changed to "Submit", because you can literally submit the form
+            button[1].setAttribute("type", "submit"); // Form can be submit
+        };
+    };
+};
+
 
 // When the left button ('Terug') gets hit
 button[0].onclick = () => {
     if (current_fieldset > 0) {
         current_fieldset--;
         var back_fieldset = current_fieldset + 1;
-        fieldset[current_fieldset].style.display = "block";
-        fieldset[back_fieldset].style.display = "none";
-        li[current_fieldset].style.backgroundColor = "red";
+        fieldset[current_fieldset].style.display = "block"; // This fieldset is now visible
+        fieldset[back_fieldset].style.display = "none"; // This fieldset is now invisible
+        li[current_fieldset].style.backgroundColor = "red"; // The current bullet of the procesbar gets a red color
         li[back_fieldset].style.backgroundColor = "#b3b3b3";
         if (current_fieldset < 2) {
-            button[1].innerHTML = "Verder";
-            button[1].setAttribute("type", "button")
-        }
-    }
+            button[1].innerHTML = "Verder"; // Button text is changed to "Verder", because you can tap through the form fieldsets
+            button[1].setAttribute("type", "button"); // Prevent turning back to the home page
+        };
+    };
 
-    // Wanneer user alleen op fieldset[0] is en naar home wilt gaan.
-    button[0].addEventListener('click', function home() {
-        if (current_fieldset == 0) {
-            if (button[0].onclick = function goHome() {
-                location.href = ('index.html')
-            });
-        }
-    });
-}
+    // When user is on fieldset[0] and wants to go to the home page, he presses the back button
+    if (current_fieldset == 0) {
+        button[0].addEventListener('click', function home() {
+            location.href = ('index.html'); // Destination of the page; home page
+        });
+    };
+};
