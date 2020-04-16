05e12261fd38d2f13834c9f8d36f8bdafa215328
ralfz123 <ralf_zonneveld@hotmail.com>
2020-04-17 01:35:39 +0200

    Cleaned up the code and added comments

--- script/profile.js
+++ script/profile.js
@@ -3,29 +3,29 @@ const enableButton = document.getElementById('enableButton');
 const inputs = document.getElementsByTagName('input')
 
 
-disablen();
+disablen(); // Invoke the function, so the disabled attribute is set to true
 
 function disablen() {
     var inputs = document.getElementsByTagName("input");
     for (var i = 0; i < inputs.length; i++) {
         inputs[i].disabled = true;
-    }
-}
+    };
+};
 
 // Functions
-enableButton.onclick = () => {
-    console.log('Clicked')
+enableButton.onclick = () => { // When user presses on the button
+    console.log('Clicked');
 
     for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].type == 'text' || inputs[i].type == 'password' || inputs[i].type == 'number' || inputs[i].type == 'radio')
-            if (inputs[i].disabled != false) {
-                inputs[i].disabled = false;
+        if (inputs[i].type == 'text' || inputs[i].type == 'password' || inputs[i].type == 'number' || inputs[i].type == 'radio') // Activate all kinds of input fields
+            if (inputs[i].disabled != false) { // When it's not equal to false (so true)
+                inputs[i].disabled = false; // Remove "disabled" attribute
                 console.log('Removed "disabled"');
-                enableButton.innerHTML = 'Opslaan';
-            } else {
+                enableButton.innerHTML = 'Opslaan'; // Change button text to "Opslaan"
+            } else { 
                 console.log("Disabled!")
-                enableButton.innerHTML = 'Bewerk'
-                inputs[i].disabled = true;
-            }
-    }
-}
+                enableButton.innerHTML = 'Bewerk'; // Change button text to "Bewerk"
+                inputs[i].disabled = true; // Set "disabled" attribute to true
+            };
+    };
+};
